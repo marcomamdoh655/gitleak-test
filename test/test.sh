8ed8b96f4213eb5cd3af21547ac78674e4f01cf8
marcomamdoh655 <113299903+marcomamdoh655@users.noreply.github.com>
2023-01-10 13:34:53 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -3,4 +3,4 @@
 export REACT_APP_AUTH0CLIENTID="8xg4UQFxlNRVReQHeW47RM65pQSzaOh1"
 export REACT_APP_AUTH0CLIENTID=${{secrets.REACT_APP_AUTH0CLIENTID}}
 
-# @generated
+# @generated  # @not-generated
