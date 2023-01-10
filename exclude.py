429d1525cb4a8650da79159cd2bf59245a21088b
marcomamdoh655 <113299903+marcomamdoh655@users.noreply.github.com>
2023-01-10 22:59:26 +0200

    Update exclude.py

--- exclude.py
+++ exclude.py
@@ -1,3 +1,7 @@
-with open('filestobeexcluded.txt', 'r') as f:
-    lines = [line.replace('\n', '').strip() for line in f.readlines() if line.replace('\n', '').strip()]
+with open("filestobeexcluded.txt", "r") as f:
+    lines = [
+        line.replace("\n", "").strip()
+        for line in f.readlines()
+        if line.replace("\n", "").strip()
+    ]
     print("|".join(lines))
